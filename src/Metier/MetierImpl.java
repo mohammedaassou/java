50c25144c85f104b5985ee6becf4a0ec5c7b8e28
Mohamed Aassou <mohamedaassou12@gmail.com>
2025-10-07 22:01:20 +0100

    feat: implement IMetier interface and its MetierImpl class

---
+++ src/Metier/MetierImpl.java
@@ -0,0 +1,23 @@
+package Metier;
+
+import Dao.IDao;
+
+public class MetierImpl implements IMetier {
+    private IDao dao;
+
+    public MetierImpl() {}
+
+    public MetierImpl(IDao dao) {
+        this.dao = dao;
+    }
+
+    public void setDao(IDao dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public double calcul() {
+        double data = dao.getData();
+        return data * 23;
+    }
+}
