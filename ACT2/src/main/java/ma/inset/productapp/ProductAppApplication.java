0ea1aa37fdb5a98d91ba65825ab5deb021754cbf
Mohamed Aassou <mohamedaassou12@gmail.com>
2025-10-22 11:20:17 +0100

    ACT2: create JPA Product repositry + add new products to db

---
+++ ACT2/src/main/java/ma/inset/productapp/ProductAppApplication.java
@@ -0,0 +1,31 @@
+package ma.inset.productapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import ma.inset.productapp.entities.Product;
+import ma.inset.productapp.repository.ProductRepository;
+
+@SpringBootApplication
+public class ProductAppApplication implements  CommandLineRunner {
+    @Autowired
+    ProductRepository productRepository;
+    public static void main(String[] args) {
+        SpringApplication.run(ProductAppApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        productRepository.save(new Product(null,"Computer",4500.0,5));
+        productRepository.save(new Product(null,"Printer",800.0,8));
+        productRepository.save(new Product(null,"Smartphone",1200.0,15));
+
+        productRepository.findAll().forEach(p->{
+            System.out.println(p.toString());
+        });
+        
+    }
+
+}
